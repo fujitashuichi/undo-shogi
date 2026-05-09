d2959cb6ae48c202e715d263a95cbe0912406353
fujitashuichi <fujitashuu14@gmail.com>
2026-05-09 11:18:10 +0900

    feature: 駒の所有権交代を実装

--- packages/core/entities/Piece.ts
+++ packages/core/entities/Piece.ts
@@ -1,15 +1,19 @@
+import type { UUID } from "crypto";
 import { logger } from "../../tools/index.js";
 import type { PieceKind, Side } from "./types.js";
 import { pieceValidator } from "./validators/PieceValidator.js";
 
 
 export class ShogiPiece {
+  public readonly id: UUID;
+
   constructor(
     public readonly side: Side,
     public readonly kind: PieceKind,
     public readonly isPromoted: boolean = false
   ) {
     pieceValidator(isPromoted, kind);
+    this.id = crypto.randomUUID();
   }
 
 
@@ -20,5 +24,13 @@ export class ShogiPiece {
     }
 
     return new ShogiPiece(this.side, this.kind, true);
+  }
+
+  public changeSide = (): ShogiPiece => {
+    logger.trace(`${this.side}の${this.kind}が対局相手に渡しました。`);
+
+    const nextSide: Side = this.side === "Sente" ? "Gote" : "Sente";
+
+    return new ShogiPiece(nextSide, this.kind, false);
   }
 }
