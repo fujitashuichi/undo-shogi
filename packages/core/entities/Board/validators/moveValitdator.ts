250837fee81d0dd3ba6406bef8339ea8d72a47ec
fujitashuichi <fujitashuu14@gmail.com>
2026-05-11 11:33:34 +0900

    feature: 打ち込みバリデーションと成バリデーションを追加

--- packages/core/entities/Board/validators/moveValitdator.ts
+++ packages/core/entities/Board/validators/moveValitdator.ts
@@ -1,9 +1,12 @@
+import type { TupleType } from "typescript";
 import { logger } from "../../../../tools/index.js";
 import { MovementError } from "../../../errors/movement.errors.js";
 import { boardConfig } from "../../config/boardConfig.js";
 import type { ShogiPiece } from "../../Piece/Piece.js";
 import type { Position } from "../../types/algebraic.types.js";
 import type { Board } from "../Board.js";
+import type { Side } from "../../types/piece.types.js";
+import { isInsideRange } from "../../../../tools/math/isInsideRange.js";
 
 
 const boardSize = boardConfig.boardSize;
@@ -29,5 +32,46 @@ export const moveValidator = {
       logger.error("味方の駒がいる場所には移動できません。");
       throw new MovementError("MOVE_TO_INVALID_SQUARE");
     }
+
+    return true;
+  },
+
+  canDrop: (board: Board, position: Position, piece: ShogiPiece) => {
+    const pieceInTargetSquare = board.squares[position.y]![position.x];
+
+    if (pieceInTargetSquare) {
+      logger.error("そのマスには駒が存在するため持ち駒を打てません。");
+      throw new MovementError("DROP_TO_INVALID_SQUARE");
+    }
+
+
+    if (piece.kind === "Knight") {
+      // 桂馬は相手陣地2段目以内に打てません（移動不能な駒となるため）
+      const invalidYRange: [number, number] = piece.side === "Sente" ? [0, 1] : [8, 7];
+      if (isInsideRange(position.y, invalidYRange)) {
+        throw new MovementError("DROP_TO_INVALID_SQUARE");
+      }
+    }
+
+    if (piece.kind === "Lance" || piece.kind === "Pawn") {
+      // 前にしか動けない駒は、最下段に打てません（移動不能な駒となるため）
+      const invalidY = piece.side === "Sente" ? 0 : 8;
+      if (position.y === invalidY) {
+        throw new MovementError("DROP_TO_INVALID_SQUARE");
+      }
+    }
+
+    return true;
+  },
+
+  canPromote: (board: Board, side: Side, currentPos: Position, nextPos: Position) => {
+    // 既に持ち駒が直接成らないように定義しているためバリデーションは不要
+
+    if (!board.isInPromotionZone(side, currentPos) && !board.isInPromotionZone(side, nextPos)) {
+      logger.error("相手陣地に入るか相手陣地から動かないと成りはできません。");
+      throw new MovementError("INVALID_PROMOTION");
+    }
+
+    return true;
   }
 }
