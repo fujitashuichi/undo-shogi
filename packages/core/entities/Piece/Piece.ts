9c482356a6360bab1f386df5c6423c59bd9243fe
fujitashuichi <fujitashuu14@gmail.com>
2026-05-11 10:13:14 +0900

    feature: promoteロジックの場合分けを追加

--- packages/core/entities/Piece/Piece.ts
+++ packages/core/entities/Piece/Piece.ts
@@ -1,7 +1,8 @@
 import type { UUID } from "crypto";
 import { logger } from "../../../tools/index.js";
 import type { PieceKind, Side } from "../types/piece.types.js";
-import { pieceValidator } from "./validators/PieceValidator.js";
+import { pieceValidator } from "./validators/pieceValidator.js";
+import { NoPromotablePieceSchema } from "./validators/types.js";
 
 
 export class ShogiPiece {
@@ -21,11 +22,17 @@ export class ShogiPiece {
       return this;
     }
 
+    const isNonPromotable = NoPromotablePieceSchema.safeParse(this.kind).success;
+    if (isNonPromotable) {
+      logger.warn(`${this.kind}は成ることが出来ない駒です。`);
+      return this;
+    }
+
     return new ShogiPiece(this.side, this.kind, true, this.id);
   }
 
   public changeSide = (): ShogiPiece => {
-    logger.trace(`${this.side}の${this.kind}が対局相手に渡しました。`);
+    logger.trace(`${this.side}の${this.kind}が対局相手に渡りました。`);
 
     const nextSide: Side = this.side === "Sente" ? "Gote" : "Sente";
 
