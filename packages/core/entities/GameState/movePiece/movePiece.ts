b8d2de8aa69a990e0e724d823a3461d1600d16d1
fujitashuichi <fujitashuu14@gmail.com>
2026-05-16 18:21:51 +0900

    refactor: 関数を切り出し

---
+++ packages/core/entities/GameState/movePiece/movePiece.ts
@@ -0,0 +1,24 @@
+import type { Position } from "../../types/algebraic.types.js";
+import { GameState } from "../GameState.js";
+import { positionValidator } from "../validators/positionValidator.js";
+
+export const gameState_movePiece = (gameState: GameState, current: Position, next: Position, promote: boolean) => {
+  positionValidator.assertInBoard(current.x, current.y);
+  positionValidator.assertInBoard(next.x, next.y);
+
+
+  const pieceInNextPos = gameState.board.squares[next.y]![next.x];
+  let nextHands = gameState.hands;
+
+  if (pieceInNextPos) {
+    nextHands = gameState.hands.addPiece(
+      gameState.currentSide,
+      pieceInNextPos.disPromote().kind
+    );
+  }
+
+  const nextBoard = gameState.board.movePiece(current, next, promote);
+  const nextSide = gameState.currentSide === "Sente" ? "Gote" : "Sente";
+
+  return new GameState(nextBoard, nextHands, nextSide);
+}
