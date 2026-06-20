41f387c8c4606b79dfdbbc2d2e91cb31fd4081c1
fujitashuichi <fujitashuu14@gmail.com>
2026-06-21 01:10:10 +0900

    refactor: ShogiRoomの責務を調整

--- apps/backend/ws/Groups/ShogiRoom.ts
+++ apps/backend/ws/Groups/ShogiRoom.ts
@@ -1,29 +1,111 @@
-import { ShogiController, type Handicap, type ShogiTimerOptions } from "@packages";
-import type { UUID } from "crypto";
-import { WebSocket } from "ws";
+import { DomainError, ShogiController, type Handicap, type NormalPieceKind, type ShogiTimerOptions, type Side } from "@packages";
+import type { WsClient } from "../Clients/WsClient";
+import type { Position } from "../../../../packages/dist/domain/entities/types/algebraic.types";
 
 
 type Props = {
-  gameId: UUID,
   handicap: "hirate" | Handicap,
-  timerOptions: ShogiTimerOptions,
-  ws: WebSocket
+  timerOptions: ShogiTimerOptions
 }
+
+type PlayResult =
+  | {
+    success: false,
+    code: DomainError["code"]
+  }
+  | {
+    success: true,
+    status: ShogiController["status"],
+    message: string
+  };
 
 
 export class ShogiRoom {
-  public readonly gameId;
-  public readonly ws;
-  public readonly controller;
+  private readonly _controller: ShogiController;
+
 
   constructor({
-    gameId,
     handicap,
-    timerOptions,
-    ws
+    timerOptions
   }: Props) {
-    this.gameId = gameId;
-    this.ws = ws;
-    this.controller = ShogiController.init[handicap](timerOptions);
+    this._controller = ShogiController.init[handicap](timerOptions);
+  }
+
+
+  public get currentSide() {
+    return this._controller.status.currentSide;
+  }
+
+  public get status() {
+    return this._controller.status;
+  }
+
+
+  public readonly startMatch = (): PlayResult => {
+    return this.handlePlay(
+      `Start match.`,
+      () => this._controller.start()
+    );
+  }
+
+  public readonly stopMatch = (): PlayResult => {
+    return this.handlePlay(
+      `Stop match.`,
+      () => this._controller.stop()
+    );
+  }
+
+
+  public readonly movePiece = (
+    currentPos: Position, nextPos: Position, promote: boolean
+  ): PlayResult => {
+    return this.handlePlay(
+      `${this.currentSide} moved Piece.`,
+      () => this._controller.movePiece(currentPos, nextPos, promote)
+    )
+  }
+
+  public readonly dropPiece = (
+    position: Position, piece: NormalPieceKind
+  ): PlayResult => {
+    return this.handlePlay(
+      `${this.currentSide} dropped Piece.`,
+      () => this._controller.dropPiece(position, piece)
+    )
+  }
+
+  public readonly undo = (): PlayResult => {
+    return this.handlePlay(
+      `Undo ${this.currentSide}'s play.`,
+      () => this._controller.undo()
+    )
+  }
+
+
+  private readonly handlePlay = (
+    message: string,
+    fn: () => void
+  ): PlayResult => {
+    try {
+      fn();
+
+      return {
+        success: true,
+        status: structuredClone(this._controller.status),
+        message
+      };
+    } catch (err) {
+      if (!(err instanceof DomainError)) {
+        return {
+          success: false,
+          code: "INTERNAL_ERROR"
+        };
+      }
+
+      return {
+        success: false,
+        code: err.code
+      };
+    }
   }
 }
