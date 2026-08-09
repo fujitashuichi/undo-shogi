bee3a33e9629c0d85bd3c1de9b020b84fc91547e
fujitashuu14 <fujitashuu14@gmail.com>
2026-08-09 22:12:10 +0900

    feature: コンポーネントからsocketStateを使用 (#149)

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import { SocketStatusView } from "@/features/ws/components/SocketStatusView";
+import { WebSocketProvider } from "@/features/ws/contexts/WebSocketProvider";
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-
+        <WebSocketProvider>
+          <SocketStatusView />
+        </WebSocketProvider>
       </main>
     </div>
   );
--- packages/tools/src/index.ts
+++ packages/tools/src/index.ts
@@ -1,5 +1,3 @@
-export { vitest_checkArray } from "./vitest/checkArray.js";
-
 export { logger } from "./logger/index.js";
 
 export type { Prettify } from "./types/prettify.js";
