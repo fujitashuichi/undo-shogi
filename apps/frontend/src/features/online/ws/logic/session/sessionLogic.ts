7a860c1200f47e823893faf17e0529ddea06d8c4
fujitashuichi <fujitashuu14@gmail.com>
2026-08-14 15:34:25 +0900

    feature: WebSocketの受信時ロジック

--- apps/frontend/src/features/online/ws/logic/session/sessionLogic.ts
+++ apps/frontend/src/features/online/ws/logic/session/sessionLogic.ts
@@ -5,5 +5,15 @@ export const sessionLogic = (
   message: ServerSessionMessage,
   shogiCtx: ShogiCtx
 ) => {
+  const { sessionCtx, sessionErrorCtx } = shogiCtx;
+  const { setStatus, setSession } = sessionCtx;
+  const { setErrorName } = sessionErrorCtx;
 
+  if (!message.success) {
+    setErrorName(message.errorName);
+    return;
+  }
+
+  setStatus("matched");
+  setSession(message.body);
 }
--- apps/frontend/src/features/online/ws/logic/system/systemLogic.ts
+++ apps/frontend/src/features/online/ws/logic/system/systemLogic.ts
@@ -5,5 +5,8 @@ export const systemLogic = (
   message: ServerSystemMessage,
   shogiCtx: ShogiCtx
 ) => {
+  const { systemErrorCtx } = shogiCtx;
+  const { setErrorName } = systemErrorCtx;
 
+  setErrorName(message.errorName);
 }
